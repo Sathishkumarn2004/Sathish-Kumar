9e96c44c09e8b159a6ea23740755d8b1f7cd88ce
Sathishkumarn2004 <nsathishkumar2004@gmail.com>
2024-07-15 10:38:19 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,23 @@
+package Yogeshlee;
+
+public class New_Pattern 
+{
+       public static void main(String args[])
+       {
+    	  for(int i=0; i<3; i++)
+    	  {
+    		  System.out.print("@ ");
+    	  }
+    	  System.out.println(" ");
+    	  for(int j=0; j<3; j++)
+    	  {
+    		  System.out.print("? ");
+    	  }
+    	  System.out.println(" ");
+    	  for(int z=0; z<3; z++)
+    	  {
+    		  System.out.print("> ");
+    	  }
+    	  System.out.println(" ");
+       }
+}
