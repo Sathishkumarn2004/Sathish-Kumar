a4b36aa372c243368fe95638aeec28d4721343a0
Sathishkumarn2004 <nsathishkumar2004@gmail.com>
2024-07-15 10:25:56 +0530

    Create Wrapper.java

---
+++ Wrapper.java
@@ -0,0 +1,11 @@
+public class WrapperExample1{  
+public static void main(String args[]){  
+int a=200; 
+Float b=6.0f;
+Integer i=Integer.valueOf(a);
+Integer j=a; 
+Integer z=a;
+Float   k=b;
+System.out.println(a+" "+i+" "+j +" " +z + " "+k + " "+s);  
+}
+}
