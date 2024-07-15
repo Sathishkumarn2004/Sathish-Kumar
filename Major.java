a5a73a3c8a35692e9bc08ae311d6ad8e71a57ab3
Sathishkumarn2004 <nsathishkumar2004@gmail.com>
2024-07-15 10:34:03 +0530

    Create Major.java

---
+++ Major.java
@@ -0,0 +1,24 @@
+package Yogeshlee;
+import java.util.*;
+public class Major 
+{
+		public static void main(String[] args) 
+		{
+			int a[] = new int[3];
+			String b[] = {" abc","bva","jghg"};
+			a[0] = 12;
+			a[1]= 21;
+			a[2] = 32;
+			for(int i=0; i<a.length; i++)
+			{
+				 System.out.println(a[i]);
+			}
+			System.out.println(a.length);
+			 System.out.println(a[1]);
+				for(int j=0; j<b.length; j++)
+				{
+	          System.out.println(b[j]);
+				}
+	        }
+
+		}
